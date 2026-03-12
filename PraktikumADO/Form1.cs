93619a68b18b46363aaae078b747a5d68f600d72
Faiqfarmz <faiqfarmz@gmail.com>
2026-03-12 13:34:40 +0700

    private void hitung mahasiswa

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -48,4 +48,19 @@ namespace PraktikumADO
                 MessageBox.Show("Error: " + ex.Message);
             }
         }
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+
+
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
